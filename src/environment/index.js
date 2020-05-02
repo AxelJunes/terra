346801d7f072e16da6cb6d129e6307d43cc5ba4e
Axel Junes <axjunestrand@gmail.com>
2020-05-02 19:53:13 +0200

    Updated relay config

--- src/environment/index.js
+++ src/environment/index.js
@@ -10,7 +10,7 @@ const fetchQuery = async (
   operation,
   variables
 ) => {
-  const response = await fetch('/graphql', {
+  const response = await fetch('http://localhost:4000/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
