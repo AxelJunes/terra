cf9b2c38e2b62a34e41459636205f3ac68b17498
Axel Junes <axjunestrand@gmail.com>
2020-05-02 19:45:29 +0200

    Relay environment

---
+++ src/environment/index.js
@@ -0,0 +1,32 @@
+import {
+  Environment,
+  Network,
+  RecordSource,
+  Store
+} from 'relay-runtime'
+import fetch from 'node-fetch'
+
+const fetchQuery = async (
+  operation,
+  variables
+) => {
+  const response = await fetch('/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables
+    })
+  })
+
+  return response.json()
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource())
+})
+
+export default environment
